4dedfd647d3753b3f3a5be51ae4b789d9dd8283f
Mohammad-451 <s445460451@std.su.edu.sa>
2025-09-22 10:51:33 +0300

    Add files via upload

---
+++ script 9.py
@@ -0,0 +1,9 @@
+tuble = ['abcd', 786, 2.23, 'john, 70.2']
+tinytuble = [123, 'john']
+
+print(tuble)
+print(tuble[0])
+print(tuble[1:3])
+print(tuble[2:])
+print(tinytuble *2)
+print(tuble + tinytuble)
